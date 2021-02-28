899a18be55ebb26683ef96b4f9d4bc8a720a8d08
MarkusKiesel93 <markus.kiesel@tuta.io>
2021-02-28 21:00:45 +0100

    fix startscript

--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -4,7 +4,7 @@ set -e
 
 pytest -s ./app
 
-if [[ ${DEPLOY_MODE} -eq "production" ]]; then
+if [ ${DEPLOY_MODE} = "production" ]; then
     gunicorn -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 --workers 4 app.main:app
 else
     uvicorn app.main:app --reload --host 0.0.0.0 --port ${BACKEND_PORT}
