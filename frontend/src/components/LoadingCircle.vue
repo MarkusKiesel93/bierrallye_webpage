b4754b4f9becd98f76be3258631daf03a013b529
MarkusKiesel93 <markus.kiesel@tuta.io>
2021-02-28 20:50:53 +0100

    loading component

---
+++ frontend/src/components/LoadingCircle.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container
+    fluid
+    v-if="show"
+  >
+    <v-progress-circular
+      :size="50"
+      color="primary"
+      indeterminate
+    ></v-progress-circular>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'LoadingCircle',
+  props: {
+    show: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
