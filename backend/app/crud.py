cc9261b9322a075227ed8327932c2f54d460cd26
MarkusKiesel93 <markus.kiesel@tuta.io>
2021-03-01 18:10:07 +0100

    change to places taken

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -27,14 +27,9 @@ def get_team_by_email(db: Session, email: str):
     return db.query(models.Team).filter(models.Team.email == email).first()
 
 
-def get_places_left(db: Session):
-    places_left = {
-        'block_1': 30,
-        'block_2': 30,
-        'block_3': 30,
-        'block_4': 30,
-    }
+def get_places_taken(db: Session):
+    places_taken = {}
     counts = db.query(models.Team.time_pref, func.count(models.Team.email)).group_by(models.Team.time_pref).all()
     for block, count in counts:
-        places_left[block] -= count
-    return places_left
+        places_taken[block] = count
+    return places_taken
