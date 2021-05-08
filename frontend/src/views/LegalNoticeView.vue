3a1ce98655447a44d2105071f5f32cfc308762db
MarkusKiesel93 <markus.kiesel@tuta.io>
2021-05-08 16:05:17 +0200

    update texts

--- frontend/src/views/LegalNoticeView.vue
+++ frontend/src/views/LegalNoticeView.vue
@@ -1,22 +1,29 @@
 <template>
   <v-card>
-    <v-card-title>
-      Impressum
-    </v-card-title>
+    <h2 class="py-4 text-center"> Impressum </h2>
     <v-card-text>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Eget arcu dictum
-      varius duis at consectetur lorem. A pellentesque sit amet porttitor eget
-      dolor morbi. Ut faucibus pulvinar elementum integer enim neque volutpat
-      ac. Mattis molestie a iaculis at erat pellentesque adipiscing commodo.
-      Fermentum posuere urna nec tincidunt praesent semper feugiat nibh. Massa
-      massa ultricies mi quis. Porttitor eget dolor morbi non arcu risus quis
-      varius quam. Morbi tristique senectus et netus et malesuada fames ac
-      turpis. Et leo duis ut diam quam nulla porttitor massa. Sit amet purus
-      gravida quis blandit. Auctor urna nunc id cursus. Tempus urna et pharetra
-      pharetra. Amet consectetur adipiscing elit ut aliquam. Consequat id porta
-      nibh venenatis cras sed felis. In fermentum et sollicitudin ac orci
-      phasellus egestas.
+      <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;'><strong>MEDIENINHABER&nbsp;</strong></p>
+      <p style='margin-top:0.5cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;'><strong>&nbsp;</strong></p>
+      <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;'>JVP Irnfritz</p>
+      <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;'>Haupstra&szlig;e 21A</p>
+      <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;'>3754 Irnfritz</p>
+      <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;'>AUT</p>
+      <p style='margin-top:0.5cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;'>&nbsp;</p>
+      <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;'>Vereinsobmann: Daniel Siegl</p>
+      <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;'>Telefon: 0664/2527279</p>
+      <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;'>E-Mail: <a href="mailto:bierrallye.irnfritz@gmail.com"><span style="font-size:14px;line-height:107%;font-family:Roboto;color:#1976D2;background:white;">bierrallye.irnfritz@gmail.com</span></a></p>
+      <p style='margin-top:0.5cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;'>&nbsp;</p>
+      <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;'>&nbsp;</p>
+      <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;'><strong>OFFENLEGUNG NACH &sect; 25 MEDIENGESETZ &middot; COPYRIGHT &middot; HAFTUNG</strong></p>
+      <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;'>&nbsp;</p>
+      <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;text-align:justify;'>Die grundlegende Richtung der Website&nbsp;bierrallye-irnfritz.at besteht in der Information und der Anmeldung/ Abmeldung zur Veranstaltung &bdquo;Bierrallye der JVP Irnfritz&ldquo;.</p>
+      <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;text-align:justify;'>&nbsp;</p>
+      <p style='margin-top:0.5cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;text-align:justify;'>Im Hinblick auf die technischen Eigenschaften des Internet kann keine Gew&auml;hr f&uuml;r die Authentizit&auml;t, Richtigkeit und Vollst&auml;ndigkeit der im Internet zur Verf&uuml;gung gestellten Informationen &uuml;bernommen werden. Es wird auch keine Gew&auml;hr f&uuml;r die Verf&uuml;gbarkeit oder den Betrieb der gegenst&auml;ndlichen Website&nbsp;und ihrer Inhalte &uuml;bernommen.</p>
+      <p style='margin-top:0.5cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;text-align:justify;'>Jede Haftung f&uuml;r unmittelbare, mittelbare oder sonstige Sch&auml;den, unabh&auml;ngig von deren Ursachen, die aus der Benutzung oder Nichtverf&uuml;gbarkeit der Daten und Informationen dieser Website&nbsp;erwachsen, wird, soweit rechtlich zul&auml;ssig, ausgeschlossen.</p>
+      <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;text-align:justify;'>Der Inhalt dieser Website&nbsp;ist urheberrechtlich gesch&uuml;tzt. Die Informationen sind nur f&uuml;r die pers&ouml;nliche Verwendung bestimmt. Jede weitergehende Nutzung insbesondere die Speicherung in Datenbanken, Vervielf&auml;ltigung und jede Form von gewerblicher Nutzung sowie die Weitergabe an Dritte auch in Teilen oder in &uuml;berarbeiteter Form ohne Zustimmung der jeweiligen Organisation ist untersagt.</p>
+      <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;text-align:justify;'>&nbsp;</p>
+      <p style='margin-top:0.5cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;text-align:justify;'>Hyperlinks auf bierrallye-irnfritz.at sind willkommen. Jede Einbindung einzelner Seiten unseres Angebotes in fremde Frames ist zu unterlassen.</p>
+      <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:107%;font-size:15px;font-family:"Calibri",sans-serif;'>&nbsp;</p>
     </v-card-text>
   </v-card>
 </template>
